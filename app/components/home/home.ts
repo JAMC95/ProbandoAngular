25783d5a0d608b60caf1c08b8f20c6e2a1cbe011
Jose Antonio Moral Castilla <joxe.bailen@gmail.com>
2016-08-30 17:57:12 +0200

    Separación de un Array con pipes

--- app/components/home/home.ts
+++ app/components/home/home.ts
@@ -1,20 +1,24 @@
-
 import {Component} from "angular2/core";
 import {EventsDir} from "../../directives/events";
+import {JoinPipe} from "../../pipes/joinpipe";
 
     @Component({
         selector: "my-app",
         templateUrl: 'app/components/home/app.html',
-        directives: [EventsDir]
+        directives: [EventsDir],
+        pipes: [JoinPipe]
     })
 
     export class HomePage{
         holamundo: string;
         otrotexto: string;
         otrotextomas: string;
+        colors: Array<string>;
         constructor(){
             this.holamundo = "Hola mundo, Paco";
             this.otrotexto = "Otro texto";
             this.otrotextomas = "Otro texto mas";
+            this.colors = ["rojo","amarillo","azul","naranja"];
+
         }
     }
