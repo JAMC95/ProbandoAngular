f5ef19436f0896dbd85b3c8d229780826eb7070c
Jose Antonio Moral Castilla <joxe.bailen@gmail.com>
2016-08-30 11:00:28 +0200

    Puesta en marcha del hola mundo, angular2

---
+++ app/app.component.ts
@@ -0,0 +1,11 @@
+
+import {Component} from "angular2/core";
+
+    @Component({
+        selector: "my-app",
+        template:"<h1>Hola mundo</h1>"
+    })
+
+    export class AppComponent{
+        
+    }
